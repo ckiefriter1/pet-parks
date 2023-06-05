fa7075c4926cf0efaa3d8f8bfadaf72ab56a58f1
Chuck Kiefriter <ckiefriter@gmail.com>
2023-06-04 20:39:47 -0400

    week 13

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO amenity (amenityName) VALUES ('Dog friendly');
+INSERT INTO amenity (amenityName) VALUES ('cat friendly');
+INSERT INTO amenity (amenityName) VALUES ('turlte friendly');
+INSERT INTO amenity (amenityName) VALUES ('chicken friendly');
+INSERT INTO amenity (amenityName) VALUES ('restrooms');
+INSERT INTO amenity (amenityName) VALUES ('Hot dog stand');
