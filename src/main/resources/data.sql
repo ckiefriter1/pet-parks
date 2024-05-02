2fdbfbc6f9df27459630a4ab8daf429a73784a2f
Chuck Kiefriter <ckiefriter@gmail.com>
2024-05-01 22:01:08 -0400

    week13

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,12 @@
+DELETE FROM amenity;
+DELETE FROM contributor;
+
 INSERT INTO amenity (amenity) VALUES ('Dog friendly');
 INSERT INTO amenity (amenity) VALUES ('cat friendly');
 INSERT INTO amenity (amenity) VALUES ('turlte friendly');
 INSERT INTO amenity (amenity) VALUES ('chicken friendly');
 INSERT INTO amenity (amenity) VALUES ('restrooms');
 INSERT INTO amenity (amenity) VALUES ('Hot dog stand');
+
+INSERT INTO contributor (contributor_email, contributor_name) VALUES ('ckiefriter@gmail.com', 'Chuck Kiefriter');
+INSERT INTO contributor (contributor_email, contributor_name) VALUES ('chuck@someemail.com', 'Chuck at work');
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -4,3 +4,6 @@ INSERT INTO amenity (amenity) VALUES ('turlte friendly');
 INSERT INTO amenity (amenity) VALUES ('chicken friendly');
 INSERT INTO amenity (amenity) VALUES ('restrooms');
 INSERT INTO amenity (amenity) VALUES ('Hot dog stand');
+
+-- INSERT INTO contributor (contributor_email, contributor_name) VALUES ('ckiefriter@gmail.com', 'Chuck Kiefriter');
+-- INSERT INTO contributor (contributor_email, contributor_name) VALUES ('chuck@someemail.com', 'Chuck at work');
